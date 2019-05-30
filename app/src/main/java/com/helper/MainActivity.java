8a78fa76a367be70abad0064bb1c3de3f23d4da3
SlokBaniya <slokbaniya1@gmail.com>
2019-05-30 21:55:21 +0545

    Add Login and register activity

--- app/src/main/java/com/helper/MainActivity.java
+++ app/src/main/java/com/helper/MainActivity.java
@@ -1,13 +1,41 @@
 package com.helper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+private Button btnGoTOLogin, btnGoToRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+          btnGoToRegister = findViewById(R.id.btnGoToRegister);
+        btnGoToLogin = findViewById(R.id.btGoTonLogin);
+
+        btnGoToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
